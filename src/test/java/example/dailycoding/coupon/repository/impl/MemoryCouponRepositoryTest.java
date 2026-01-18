76cc209c65e30963f4b2d7aca60ed627c1bac60e
hyun <60086998+hyun1202@users.noreply.github.com>
2026-01-18 14:38:14 +0900

    [Continue] Day 1: 새 쿠폰 생성 API  (#2)
    * [Continue] Day 1: 새 쿠폰 생성 API Controller 추가
    
    * [Continue] Day 1: 새 쿠폰 생성 API memory repository Test 추가
    
    * [Continue 수정] Day 1: 새 쿠폰 생성 API memory repository Test 비교 변경
    
    - hasValue(coupon)은 객체의 참조 동등성을 비교하므로
    extracting을 이용하여 객체 필드 값 비교하도록 변경
    
    * [Continue 수정] Day 1: 새 쿠폰 생성 요청 API 및 test 수정
    
    - 요청 api 경로 변경
    - 테스트 독립성 보장을 위해 각 테스트마다 repository 새로 초기화하도록 수정

--- src/test/java/example/dailycoding/coupon/repository/impl/MemoryCouponRepositoryTest.java
+++ src/test/java/example/dailycoding/coupon/repository/impl/MemoryCouponRepositoryTest.java
@@ -1,7 +1,86 @@
 package example.dailycoding.coupon.repository.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import example.dailycoding.coupon.domain.Coupon;
+import example.dailycoding.coupon.domain.DiscountType;
+import example.dailycoding.coupon.repository.CouponRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MemoryCouponRepositoryTest {
+    CouponRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        repository = new MemoryCouponRepository();
+    }
+
+    @Test
+    @DisplayName("생성된 쿠폰을 가져온다.")
+    void getCoupon() {
+        // given
+        String uuid = UUID.randomUUID().toString();
+        String name = "coupon1";
+        BigDecimal discount = BigDecimal.valueOf(10);
+        LocalDateTime startDate = LocalDateTime.of(2026, 1, 18, 2, 11);
+        LocalDateTime endDate = startDate.plusDays(10);
+
+        Coupon coupon = Coupon.builder()
+                .uuid(uuid)
+                .name(name)
+                .discount(discount)
+                .startDate(startDate)
+                .endDate(endDate)
+                .build();
+
+        // when
+        repository.addCoupon(coupon);
+        Optional<Coupon> result = repository.getCoupon(uuid);
+
+        assertThat(result)
+                .isPresent()
+                .get()
+                .extracting(Coupon::getId, Coupon::getName, Coupon::getDiscount,
+                            Coupon::getStartDate, Coupon::getEndDate)
+                .containsExactly(uuid, name, discount, startDate, endDate);
+    }
+
+    @Test
+    @DisplayName("쿠폰을 생성한다.")
+    void addCoupon() {
+        // given
+        String uuid = UUID.randomUUID().toString();
+        String name = "coupon1";
+        BigDecimal discount = BigDecimal.valueOf(10);
+        LocalDateTime startDate = LocalDateTime.of(2026, 1, 18, 2, 11);
+        LocalDateTime endDate = startDate.plusDays(10);
+
+        Coupon coupon = Coupon.builder()
+                .uuid(uuid)
+                .name(name)
+                .discount(discount)
+                .startDate(startDate)
+                .endDate(endDate)
+                .build();
+
+        // when
+        Coupon savedCoupon = repository.addCoupon(coupon);
+
+        // then
+        assertThat(savedCoupon).extracting(
+                Coupon::getId, Coupon::getName, Coupon::getDiscount,
+                Coupon::getDiscountType, Coupon::getStartDate, Coupon::getEndDate)
+                .containsExactly(
+                        uuid, name, discount,
+                        DiscountType.AMOUNT, startDate, endDate
+                );
+    }
 
 }
