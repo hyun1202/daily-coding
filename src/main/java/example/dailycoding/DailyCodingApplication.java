1910f67812a619d23f40f330714e9d16dc98b18a
hyun <apfhd0257@naver.com>
2026-01-18 12:13:44 +0900

    init

---
+++ src/main/java/example/dailycoding/DailyCodingApplication.java
@@ -0,0 +1,13 @@
+package example.dailycoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyCodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyCodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/example/dailycoding/DailyCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package example.dailycoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyCodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
